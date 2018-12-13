0215e0b3e76515753d42cb3221d8acba96059077
Jacob Mink <jacobmink@gmail.com>
2018-12-13 08:50:25 -0700

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// VERBAL QUESTIONS
+// 1) Difference between parameter and argument
+// 2) The difference within a function between return and console.log is that 
+//      console.log will just print something to the console, whereas return
+//      will spit out the result of the function. You can return this result
+//      into a variable for use later in the code.
+// 3) The fact that functions can return values makes it much easier to write DRY
+//      code. Any time you want to get the function's value, you can just call
+//      that function, instead of rewriting it every time.
